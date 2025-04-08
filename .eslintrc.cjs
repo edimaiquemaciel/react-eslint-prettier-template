54e200fd205cf3d29fcd054d77800317611dfcda
Edimaique <edimaiqueacacio@gmail.com>
2025-04-08 16:12:58 -0300

    feat: initial ESLint + Prettier template

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  plugins: ["import"],
+  rules: {
+    "arrow-parens": ["error", "as-needed"],
+    "import/order": [
+      "error",
+      {
+        "groups": [
+          "builtin",
+          "external",
+          "internal",
+          ["parent", "sibling"],
+          "index",
+          "object",
+          "type"
+        ],
+        "newlines-between": "always",
+        "alphabetize": {
+          "order": "asc",
+          "caseInsensitive": true
+        }
+      }
+    ]
+  }
+};
---
+++ eslint.config.js
@@ -0,0 +1,50 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import prettier from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier/recommended';
+
+export default [
+  { ignores: ['dist', '**/*.config.js'] }, // Ignora dist e arquivos de configuração
+  
+  // Config base
+  js.configs.recommended,
+  
+  // Config para arquivos JS/JSX
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+      },
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    }
+  },
+
+  // Integração com Prettier (DEVE ser o último item do array)
+  prettier,
+  prettierPlugin,
+];
